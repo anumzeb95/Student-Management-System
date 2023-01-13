f6b14b926837012e93cd8121f661ffff802b41ef
zebbbanu <zebbbanu@CRIBV-DNET3>
2023-01-13 17:58:56 +0500

    Class Details Model

---
+++ Student_Management_System/Models/ClassDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Student_Management_System.Models
+{
+    public class ClassDetailsModel
+    {
+        [ForeignKey("Student")]
+        public int Std_Id { get; set; }
+
+        [ForeignKey("Course")]
+        public int Course_Id { get; set; }
+        public int GPA { get; set; }
+    }
+}
